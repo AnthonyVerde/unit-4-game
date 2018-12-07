90fe7e350f4e7e0bb65ed2cd92f752b65fa1b475
Verde <a.green.designer@gmail.com>
2018-12-07 15:35:50 -0500

    further editing to index.html, style.css, game.js to get the game functioning properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,20 @@
 
 
+
 $(document).ready(function () {
 
-    var randomNumber = Math.floor(Math.random() * 160) + 1;
+
+    var randomNumber = Math.floor(Math.random() * 120) + 19;
     $("#numberGuess").text(randomNumber);
 
-    var yourScore = 0;
-    $("#your-score").text(yourScore);
+    var yourScore = "";
+    $("#your-score").text(yourScore = 0);
+
+    var wins = "";
+    $("#wins-text").text(wins = 0);
+
+    var livesLeft = "";
+    $("lives-text").text(livesLeft = 5);
 
     var crystal = $(".crystal-img");
     crystal.each(function () {
@@ -15,7 +23,6 @@ $(document).ready(function () {
         $(this).attr({
             "data-crystal": random
         });
-
     });
 
     $(".crystal-img").on("click", function () {
@@ -24,14 +31,88 @@ $(document).ready(function () {
         crystalValue = parseInt(crystalValue);
 
         yourScore += crystalValue;
+        $("#your-score").text(yourScore);
 
         if (yourScore === randomNumber) {
-            
+
+            $("#wins-text").text(wins += 1);
+
+            $("#your-score").text(yourScore = 0);
+
+            randomNumber = Math.floor(Math.random() * 120) + 19;
+            $("#numberGuess").text(randomNumber);
+
+            var crystal = $(".crystal-img");
+            crystal.each(function () {
+                var random = Math.floor(Math.random() * 12) + 1;
+                console.log(random);
+                $(this).attr({
+                    "data-crystal": random
+                });
+            });
+            alert("You win!");
+
+            if (wins === 5) {
+                $("lives-text").text(livesLeft = 5);
+                $("#wins-text").text(wins = 0);
+                $("#your-score").text(yourScore = 0);
+                var crystal = $(".crystal-img");
+                crystal.each(function () {
+                    var random = Math.floor(Math.random() * 12) + 1;
+                    console.log(random);
+                    $(this).attr({
+                        "data-crystal": random
+                    });
+
+                });
+                alert("You are the Crystal Master");
+            }
+
         }
 
+
         else if (yourScore >= randomNumber) {
-            
+
+            $("#lives-text").text(livesLeft -= 1);
+
+            $("#your-score").text(yourScore = 0);
+
+            randomNumber = Math.floor(Math.random() * 120) + 19;
+            $("#numberGuess").text(randomNumber);
+
+            var crystal = $(".crystal-img");
+            crystal.each(function () {
+                var random = Math.floor(Math.random() * 12) + 1;
+                console.log(random);
+                $(this).attr({
+                    "data-crystal": random
+                });
+            });
+
+            alert("You lose!");
+
+            if (livesLeft === 0) {
+                $("lives-text").text(livesLeft = 5);
+                $("#wins-text").text(wins = 0);
+                $("#your-score").text(yourScore = 0);
+                randomNumber = Math.floor(Math.random() * 120) + 19;
+                $("#numberGuess").text(randomNumber);
+                alert("You did not achieve Master Crystal Collector status");
+
+                var crystal = $(".crystal-img");
+                crystal.each(function () {
+                    var random = Math.floor(Math.random() * 12) + 1;
+                    console.log(random);
+                    $(this).attr({
+                        "data-crystal": random
+                    });
+
+                });
+            }
         }
-        console.log(yourScore);
-    });
+});
+
+document.getElementById("wins-text").innerHTML = wins;
+document.getElementById("lives-text").innerHTML = livesLeft;
+
 });
